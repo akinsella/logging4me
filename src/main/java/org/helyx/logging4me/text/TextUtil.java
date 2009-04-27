5b9169d85f2bae648c9c4ac981ed84b5986edda6
Alexis Kinsella <alexis.kinsella@gmail.com>
2009-04-27 19:15:15 +0000

    Removing unused methods

--- src/main/java/org/helyx/logging4me/text/TextUtil.java
+++ src/main/java/org/helyx/logging4me/text/TextUtil.java
@@ -19,41 +19,6 @@ import java.util.Vector;
 
 public class TextUtil {
 
-	public static String[] split(final String str, final char c) {
-
-		final Vector result = new Vector();
-		
-		int pos = 0;
-		while (true) {
-			final int cut = str.indexOf(c, pos);
-			if (cut == -1) {
-				break;
-			}
-			result.addElement(str.substring(pos, pos + cut));
-			pos = cut + 1;
-		}
-
-		String[] stringArray = new String[result.size()];
-		result.copyInto(stringArray);
-		
-		return stringArray;
-	}
-
-	public static String urlEncode(String str) {
-		StringBuffer buf = new StringBuffer();
-		char c;
-		for (int i = 0; i < str.length(); i++) {
-			c = str.charAt(i);
-			if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
-				buf.append(c);
-			}
-			else {
-				buf.append("%").append(Integer.toHexString((int)str.charAt(i)));
-			}
-		}
-		return buf.toString();
-	}
-
 	public static String replaceAll(String haystack, char needle, char replacement) {
 		int length = haystack.length();
 		
@@ -98,32 +63,6 @@ public class TextUtil {
 
 			searchPos = pos + needle.length();
 		}
-
-		result.append(haystack.substring(searchPos));
-
-		return result.toString();
-	}
-	
-	public static String replaceFirst(String haystack, String needle, String replacement) {
-		int needleLength = needle.length();
-		
-		if (needle == null || needleLength == 0) {
-			return haystack;
-		}
-
-		StringBuffer result = new StringBuffer();
-
-		int searchPos = 0;
-
-		final int pos = haystack.indexOf(needle, searchPos);
-		if (pos == -1) { 
-			return haystack;
-		}
-
-		result.append(haystack.substring(searchPos, pos));
-		result.append(replacement);
-
-		searchPos = pos + needle.length();
 
 		result.append(haystack.substring(searchPos));
 
