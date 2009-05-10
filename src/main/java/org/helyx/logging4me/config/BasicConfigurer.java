6f053d9f059ba004c3d5f716d7b85a40493f15f4
Alexis Kinsella <alexis.kinsella@gmail.com>
2009-05-10 17:58:22 +0000

    Add of License terms and corrections

--- src/main/java/org/helyx/logging4me/config/BasicConfigurer.java
+++ src/main/java/org/helyx/logging4me/config/BasicConfigurer.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2007-2009 Alexis Kinsella - http://www.helyx.org - <Helyx.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.helyx.logging4me.config;
 
 import org.helyx.logging4me.Logger;
@@ -7,11 +22,13 @@ import org.helyx.logging4me.layout.SimpleLayout;
 
 public class BasicConfigurer implements LoggerConfigurer {
 
-	private BasicConfigurer() {
+	public BasicConfigurer() {
 		super();
 	}
 	
 	public void configure() {
+		LoggerManager.resetAll();
+		
 		LoggerManager.setThresholdLevel(Logger.DEBUG);
 		
 		ConsoleAppender consoleAppender = new ConsoleAppender();
@@ -19,6 +36,8 @@ public class BasicConfigurer implements LoggerConfigurer {
 		consoleAppender.setThresholdLevel(Logger.DEBUG);
 		
 		LoggerManager.registerAppender(consoleAppender);
+		
+		LoggerManager.getRootCategory().addAppender(consoleAppender);
 	}
 
 }
